284cd9aa93517af507be4d625e98207a28f3e64e
Aurèle Ferotin <aurele.ferotin@icloud.com>
2026-02-27 13:57:37 +0100

    Add configurable username/passwords through `.env` file

--- oapif_demo/oapif_demo/management/commands/bootstrap.py
+++ oapif_demo/oapif_demo/management/commands/bootstrap.py
@@ -1,10 +1,10 @@
-
+import os
 
 from django.contrib.auth.models import Permission, User
 from django.contrib.contenttypes.models import ContentType
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
-from oapif_demo.models import Apiary, Area, PollenConsumption, Reviews, Tracks
+from oapif_demo.models import Apiary, Area, PollenConsumption, Review, Track
 
 
 class Command(BaseCommand):
@@ -17,16 +17,34 @@ class Command(BaseCommand):
         call_command("createsuperuser", "--no-input")
         call_command("loaddata", "data.json")
 
-        user = User.objects.create_user(username="user", password="123")
-        readonly_user = User.objects.create_user(username="readonly_user", password="123")
-        for model in [Apiary, Area, Tracks, Reviews, PollenConsumption]:
+        readwrite_user = User.objects.create_user(
+            username=os.getenv("DJANGO_READWRITE_USER_USERNAME"),
+            password=os.getenv("DJANGO_READWRITE_USER_PASSWORD"),
+            is_staff=True,
+        )
+        readonly_user = User.objects.create_user(
+            username=os.getenv("DJANGO_READONLY_USER_USERNAME"),
+            password=os.getenv("DJANGO_READONLY_USER_PASSWORD"),
+            is_staff=True,
+        )
+        for model in [Apiary, Area, Track, Review, PollenConsumption]:
             ct = ContentType.objects.get_for_model(model)
-            read_perm = Permission.objects.get(content_type=ct, codename=f"view_{model._meta.model_name}")
-            add_perm = Permission.objects.get(content_type=ct, codename=f"add_{model._meta.model_name}")
-            del_perm = Permission.objects.get(content_type=ct, codename=f"delete_{model._meta.model_name}")
-            change_perm = Permission.objects.get(content_type=ct, codename=f"change_{model._meta.model_name}")
-            
+            read_perm = Permission.objects.get(
+                content_type=ct, codename=f"view_{model._meta.model_name}"
+            )
+            add_perm = Permission.objects.get(
+                content_type=ct, codename=f"add_{model._meta.model_name}"
+            )
+            del_perm = Permission.objects.get(
+                content_type=ct, codename=f"delete_{model._meta.model_name}"
+            )
+            change_perm = Permission.objects.get(
+                content_type=ct, codename=f"change_{model._meta.model_name}"
+            )
+
             readonly_user.user_permissions.add(read_perm)
-            user.user_permissions.add(read_perm, add_perm, del_perm, change_perm)
+            readwrite_user.user_permissions.add(
+                read_perm, add_perm, del_perm, change_perm
+            )
 
         readonly_user.save()
