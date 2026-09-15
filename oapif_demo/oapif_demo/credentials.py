57c43e2c8fd04a26d5d8c4a8fd0a743a04d03e88
Aurèle Ferotin <aurele.ferotin@icloud.com>
2026-09-15 10:54:57 +0200

    Add login buttons to login page

--- oapif_demo/oapif_demo/credentials.py
+++ oapif_demo/oapif_demo/credentials.py
@@ -1,5 +1,6 @@
 import os
 
+from django.conf import settings
 from django.http import HttpRequest
 
 LOGIN_CREDENTIALS = {
@@ -7,6 +8,11 @@ LOGIN_CREDENTIALS = {
     for user in ("SUPERUSER", "READONLY_USER", "READWRITE_USER")
 }
 
-
 def context_processor(request: HttpRequest):
-    return {"LOGIN_CREDENTIALS": LOGIN_CREDENTIALS}
+    if settings.DEBUG:
+        return { "LOGIN_CREDENTIALS": LOGIN_CREDENTIALS }
+
+    credentials = LOGIN_CREDENTIALS.copy()
+    del credentials[os.getenv("DJANGO_SUPERUSER_USERNAME")]
+    return {"LOGIN_CREDENTIALS": credentials}
+
